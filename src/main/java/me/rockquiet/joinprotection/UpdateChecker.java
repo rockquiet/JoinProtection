bdca60de40178a3d42fa5d470a008b388300f56d
rockquiet <116979722+rockquiet@users.noreply.github.com>
2023-06-15 17:53:27 +0200

    update update checker
    Signed-off-by: rockquiet <116979722+rockquiet@users.noreply.github.com>

--- src/main/java/me/rockquiet/joinprotection/UpdateChecker.java
+++ src/main/java/me/rockquiet/joinprotection/UpdateChecker.java
@@ -1,24 +1,40 @@
 package me.rockquiet.joinprotection;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class UpdateChecker {
 
     public void check(JoinProtection plugin) throws IOException {
-        URL url = new URL("https://github.com/rockquiet/joinprotection/releases/latest");
-        HttpURLConnection con;
-        con = (HttpURLConnection) url.openConnection();
-        con.setInstanceFollowRedirects(false);
+        URL obj = new URL("https://api.github.com/repos/rockquiet/joinprotection/releases/latest");
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-        if (con.getHeaderField("Location") == null) {
+        if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
             plugin.getLogger().info("Unable to check for updates...");
             return;
         }
 
-        String[] split = con.getHeaderField("Location").split("/");
-        String latestVersion = (split[split.length - 1]).replaceFirst("^v", "");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            response.append(line);
+        }
+        bufferedReader.close();
+
+        Gson gson = new Gson();
+        JsonObject jsonResponse = gson.fromJson(response.toString(), JsonObject.class);
+
+        String latestVersion = jsonResponse.get("tag_name").getAsString().replaceFirst("^v", "");
         String currentVersion = plugin.getDescription().getVersion();
 
         if (!currentVersion.equalsIgnoreCase(latestVersion)) {
