c50f801c40054ce6eb6dde1092c0587dc38a0085
rockquiet <116979722+rockquiet@users.noreply.github.com>
2023-07-30 21:54:20 +0200

    add first join only toggle [#3]
    Signed-off-by: rockquiet <116979722+rockquiet@users.noreply.github.com>

--- src/main/java/me/rockquiet/joinprotection/listeners/JoinListener.java
+++ src/main/java/me/rockquiet/joinprotection/listeners/JoinListener.java
@@ -1,5 +1,6 @@
 package me.rockquiet.joinprotection.listeners;
 
+import me.rockquiet.joinprotection.JoinProtection;
 import me.rockquiet.joinprotection.ProtectionHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,9 +9,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
+    private final JoinProtection plugin;
     private final ProtectionHandler protectionHandler;
 
-    public JoinListener(ProtectionHandler protectionHandler) {
+    public JoinListener(JoinProtection joinProtection,
+                        ProtectionHandler protectionHandler) {
+        this.plugin = joinProtection;
         this.protectionHandler = protectionHandler;
     }
 
@@ -21,6 +25,10 @@ public class JoinListener implements Listener {
         if (!player.hasPermission("joinprotection.use")) return;
 
         if (!protectionHandler.isEnabledInWorld(player.getWorld()) && !player.hasPermission("joinprotection.bypass.world-list")) {
+            return;
+        }
+
+        if (player.hasPlayedBefore() && plugin.getConfig().getBoolean("plugin.first-join-only")) {
             return;
         }
 
